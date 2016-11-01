579b73d146548518765dcee90cffac599e732948
Vladimir Kim <rtx2014@gmail.com>
2016-11-02 00:32:48 +0300

    Add support for CHANGE_SEARCH_TEXT

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,9 +5,28 @@ console.log('Starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => {
   //state = state || {name: 'Anonymous'};
 
-  return state;
+  console.log('New action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      break;
+    default:
+      return state;
+  }
 };
+
 var store = redux.createStore(reducer);
 
+var currentState = store.getState();
+console.log('currentState', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Vladimir'
+};
+store.dispatch(action);
 var currentState = store.getState();
 console.log('currentState', currentState);
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,8 +8,24 @@ var stateDefault = {
   todos: []
 };
 var reducer = (state = stateDefault, action) => {
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
   return state;
 };
 var store = redux.createStore(reducer);
 
-console.log('currentState', store.getState());
+console.log('currentState before dispatch', store.getState());
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Fuck you!'
+});
+
+console.log('currentState after dispatch', store.getState());

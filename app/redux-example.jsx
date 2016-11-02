dbff7d5c49baba2a81f1462de87b13178d8a379a
Vladimir Kim <rtx2014@gmail.com>
2016-11-03 00:59:23 +0300

    Add adding/removing actions for hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   //state = state || {name: 'Anonymous'};
 
   // console.log('New action', action);
@@ -12,7 +20,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
-      break;
+      case 'ADD_HOBBY':
+        return {
+          ...state,
+          hobbies: [
+            ...state.hobbies,
+            {
+              id: nextHobbyId++,
+              hobby: action.hobby
+            }
+          ]
+        };
+        case 'REMOVE_HOBBY':
+          return {
+            ...state,
+            hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+          };
+      case 'ADD_MOVIE':
+        return {
+          ...state,
+          movies: [
+            ...state.movies,
+            {
+              id: nextMovieId++,
+              title: action.title,
+              genre: action.genre
+            }
+          ]
+        };
+      case 'REMOVE_MOVIE':
+        return {
+          ...state,
+          movies: state.movies.filter((movie) => movie.id !== action.id)
+        };
     default:
       return state;
   }
@@ -25,6 +65,7 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+  console.log('New state', store.getState());
 });
 
 var currentState = store.getState();
@@ -38,6 +79,38 @@ store.dispatch({
 // unsubscribe();
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Mad Max',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Fucking'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Interstellar',
+  genre: 'Si-Fi'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Margarita'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
